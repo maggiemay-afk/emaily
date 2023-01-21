9d6ae0975da43154c914d02dd8750c1091201d15
maggiemay-afk <hermsmk@gmail.com>
2023-01-21 12:17:21 -0500

    enabling cookies, added serialize and deserialize user

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 //could also be: require('./models/user'); 
 const user = require('./models/user');
@@ -12,6 +14,17 @@ const authRoutes = require('./routes/authRoutes');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
